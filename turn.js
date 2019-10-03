c303a357949b240ea398986e9b28cc93dfe05fd3
= <=>
2019-10-03 13:49:49 +0300

    make turn work

--- turn.js
+++ turn.js
@@ -1,29 +1,19 @@
 AFRAME.registerComponent('turn', {
 
-  init: function () {
-    var el = this.el;
-    var text = document.createElement('a-text');
-    text.setAttribute('value', "0");
-    text.setAttribute('position', "0 0 9.5");
-    text.setAttribute('color', "black");
-    el.appendChild(text);
-    
-  },
   tick: (function () {
+	const SPEED = 0.025;
+	const ANGLE = Math.PI / 2;
 	var rig = document.querySelector('#rig');
-    var text = this.el.querySelector('a-text');
+	var cam = document.querySelector('a-camera');
+	var rot = cam.object3D.rotation.y;
 
-	var q = new THREE.Quaternion();
-	var rotation = new THREE.Euler();
-	this.el.object3D.getWorldQuaternion(q);
-	rotation.setFromQuaternion(q);
-	var rot = rotation.z - rig.object3D.rotation.y
-
-    text.setAttribute('value', Math.round(rot * 100) / 100);
-	if (Math.abs(rotation.z) > Math.PI / 2) {
+	if (Math.abs(rot) > ANGLE) {
 		if (rot > 0) {
-		rig.object3D.rotation.y = rig.object3D.rotation.y + 0.1;
+		rig.object3D.rotation.y = rig.object3D.rotation.y + SPEED;
 		}
+		else {
+		rig.object3D.rotation.y = rig.object3D.rotation.y - SPEED;
+}
 	}
   })
 });
